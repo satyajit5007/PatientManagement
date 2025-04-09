2292bb6d41353b1b32db9a2df0d1cbb637cf59e9
satyajit5007 <bhosalesatyajit5007@gmail.com>
2025-04-09 12:12:40 +0530

    mvc

---
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -0,0 +1,32 @@
+package com.pm.patientservice.mapper;
+
+import com.pm.patientservice.dto.PatientRequestDTO;
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.model.Patient;
+
+import java.time.LocalDate;
+
+public class PatientMapper {
+    public static PatientResponseDTO toDTO (Patient patient){
+        PatientResponseDTO patientDTO = new PatientResponseDTO();
+        patientDTO.setId(patient.getId().toString());
+        patientDTO.setName(patient.getName());
+        patientDTO.setAddress(patient.getAddress());
+        patientDTO.setEmail(patient.getEmail());
+        patientDTO.setDateOfBirth(patient.getDateOfBirth().toString());
+
+        return patientDTO;
+    }
+    public static Patient toModel (PatientRequestDTO patientRequestDTO){
+        Patient patient = new Patient();
+        patient.setName(patient.getName());
+        patient.setAddress(patient.getAddress());
+        patient.setEmail(patient.getEmail());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOFBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+        return patient;
+
+
+    }
+
+}
